ee31b42b44ba9ec67509fc4ff77ebfe40c484975
Anartz Mugika <mugan86@gmail.com>
2017-04-11 10:07:52 +0200

    Add value param to show in translate string and load translate with select language

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, HostListener } from '@angular/core';
 window.focus(); // make sure we are on this page before we start typing
+import {TranslateService} from '@ngx-translate/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,7 +8,15 @@ window.focus(); // make sure we are on this page before we start typing
 })
 export class AppComponent {
   title = 'app works!';
-   keyChange(event) {
+  param = {value: 'Anartz!'};
+  keyChange(event) {
     console.log("Receive event" +  event[0] + " / " + event[1]);
+  }
+  constructor(translate: TranslateService) {
+      // this language will be used as a fallback when a translation isn't found in the current language
+      translate.setDefaultLang('eu');
+
+      // the lang to use, if the lang isn't available, it will use the current loader to get them
+      translate.use('eu');
   }
 }
